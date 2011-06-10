c95cc413f00df2b3bd4620815e2f6625ffff5c5d
MapViewer <MapViewer@54134cbd-a97a-4531-b6ac-969acc2f5be5>
2011-06-10 02:00:05 +0000

    Neuer Eigenschaften Test für Keywords
    git-svn-id: https://srv21.svn-repos.de/dev489/MapViewer/branches/Integration_Courseplay@19 54134cbd-a97a-4531-b6ac-969acc2f5be5

--- mapviewer.lua
+++ mapviewer.lua
@@ -2,7 +2,7 @@
 -- Revision der letzten Änderung :     $Rev$
 -- Author der letzten Änderung   :     $Author$
 -- Date der letzten Änderung     :     $Date$
--- $URL: $
+-- $URL$
 ----
 -- Neuer Test
 ----
